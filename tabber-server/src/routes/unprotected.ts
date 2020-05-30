54fac2c104967fa14e99a4081b815d81ca9b668b
Vernon Liu <mrvernonliu@gmail.com>
2020-05-30 02:42:15 -0700

    REFACTOR/export cleanup and react port change

--- tabber-server/src/routes/unprotected.ts
+++ tabber-server/src/routes/unprotected.ts
@@ -1,6 +1,5 @@
 import Router from "koa-router";
-import { general,  } from "../controller";
-import OAuth2Controller from "../controller/oauth2";
+import { general, oauth2 } from "../controller";
 
 // this becomes a little class with its own methods for routes and db etc.
 
@@ -8,7 +7,7 @@ const unprotectedRouter: Router = new Router();
 
 // Hello World route
 unprotectedRouter.get("/api/helloworld", general.helloWorld);
-unprotectedRouter.get("/api/loginUrl", OAuth2Controller.loginUrl);
-unprotectedRouter.get("/api/token", OAuth2Controller.tokenExchange);
+unprotectedRouter.get("/api/loginUrl", oauth2.loginUrl);
+unprotectedRouter.get("/api/token", oauth2.tokenExchange);
 
 export { unprotectedRouter };
