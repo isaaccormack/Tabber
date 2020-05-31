0d5e5e827beb55f5535c61d133e37cf52ec6db0c
Vernon Liu <mrvernonliu@gmail.com>
2020-05-31 00:17:09 -0700

    REFACTOR/ added api prefix to unprotected routes

--- tabber-server/src/routes/unprotected.ts
+++ tabber-server/src/routes/unprotected.ts
@@ -1,13 +1,15 @@
 import Router from "koa-router";
 import { general, oauth2 } from "../controller";
+import {protectedRouter} from "./protected";
 
 // this becomes a little class with its own methods for routes and db etc.
 
 const unprotectedRouter: Router = new Router();
 
+unprotectedRouter.prefix("/api")
 // Hello World route
-unprotectedRouter.get("/api/helloworld", general.helloWorld);
-unprotectedRouter.get("/api/loginUrl", oauth2.loginUrl);
-unprotectedRouter.get("/api/token", oauth2.tokenExchange);
+unprotectedRouter.get("/helloworld", general.helloWorld);
+unprotectedRouter.get("/loginUrl", oauth2.loginUrl);
+unprotectedRouter.get("/token", oauth2.tokenExchange);
 
 export { unprotectedRouter };
