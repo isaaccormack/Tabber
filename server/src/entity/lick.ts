2a21eb575b70a28dce8c5098158324e23b3e66d8
Jamie St Martin <jamiestm9@hotmail.com>
2020-06-06 11:18:14 -0700

    First pass at implementing licks
    Updated user entity to reference owned licks & shared-with-me licks.
    Created lick entity according to ER diagram created 2020-06-05. Basic
    GET, POST, DELETE functionality implemented.
    Untested.

---
+++ server/src/entity/lick.ts
@@ -0,0 +1,43 @@
+import { Entity, Column, PrimaryGeneratedColumn, ManyToOne, ManyToMany, JoinTable } from "typeorm";
+import { Length } from "class-validator";
+import { User } from "./user";
+
+@Entity()
+export class Lick {
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({
+        length: 100
+    })
+    @Length(1, 100)
+    name: string;
+
+    @Column()
+    description: string;
+
+    @Column()
+    dateUploaded: Date;
+
+    @Column()
+    audioFileLocation: string;
+
+    @Column()
+    audioLength: number; // seconds
+
+    @Column()
+    tab: string; // TODO: make tab data structure
+
+    @Column()
+    tuning: string; // TODO: make tuning data structure/data type
+
+    @Column()
+    isPublic: boolean;
+
+    @ManyToOne(type => User, owner => owner.licks)
+    owner: User;
+
+    @ManyToMany(type => User, user => user.sharedWithMe)
+    @JoinTable()
+    sharedWith: User[];
+}
--- server/src/routes/protected.ts
+++ server/src/routes/protected.ts
@@ -1,5 +1,6 @@
 import Router from "koa-router";
 import { UserController } from "../controller/user";
+import { LickController } from "../controller/lick";
 
 const protectedRouter: Router = new Router();
 
@@ -12,5 +13,11 @@ protectedRouter.post("/users", UserController.createUser);
 protectedRouter.put("/users/:id", UserController.updateUser);
 protectedRouter.delete("/users/:id", UserController.deleteUser);
 protectedRouter.delete("/testusers/:id", UserController.deleteTestUser);
+
+// Lick routes
+protectedRouter.get("/licks/:id", LickController.getLick);
+protectedRouter.post("/licks", LickController.createLick);
+protectedRouter.put("/licks/:id", LickController.updateLick);
+protectedRouter.delete("/licks/:id", LickController.deleteLick);
 
 export { protectedRouter };
