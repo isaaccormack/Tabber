29480dc93c1c72f273bbedaf6cc36a60b64863f0
Jamie St Martin <jamiestm9@hotmail.com>
2020-06-16 17:23:12 -0700

    Added basic conversion to notes/string/fret
    Added conversion from frequency to note and note to string/fret.
    Notes use MIDI note number representations, which seems pretty sensible.
    We could extend that to be used as our tuning format (i.e. store a
    6-element array, each of which is a MIDI note number representing the
    open note for strings 1 to 6).
    
    Additionally, a super-simple local maxima calculation was added to find
    local amplitude maxima in order to find where notes were played. This
    works really poorly right now; it should use a smoother range rather
    than only needing to be above the previous and next indexes. However,
    it's a good start.
    
    Note-to-string/fret conversion works alright, but doesn't at all take
    into account tuning or other recent notes. It will have to be changed to
    be contextual; i.e. be based on capo, tuning, and other nearby notes.

--- server/src/controller/tab-tmp.ts
+++ server/src/controller/tab-tmp.ts
@@ -25,6 +25,68 @@ class AudioData {
     peakAmplitude: number[];
 }
 
+class StringFret {
+    string_idx: number; // 1-6 (low-high) (I love numeric variables called string! So clear!)
+    fret: number; // 0-12
+}
+
+function getNote(pitch: number): number {
+    // Convert a pitch to a number between 0 and 127
+    // see en.wikipedia.org/wiki/MIDI_tuning_standard#Frequency_values
+    return Math.round(69 + 12 * Math.log2(pitch / 440.0));
+}
+
+// note is a MIDI-tuning-standard note index, between 0 (C_{-1}) and 127 (G_9).
+// Low E on guitar is E_2 (80 Hz, index 40).
+// For now, we take the trivial idea that the note is played on the highest-pitched
+// string that can play it. We do not take tuning into account (i.e. we assume standard tuning).
+// Standard tuning: (string index, note, note index)
+// 0 E_4 64
+// 1 B_3 59
+// 2 G_3 55
+// 3 D_3 50
+// 4 A_2 45
+// 5 E_2 40
+//
+// see en.wikipedia.org/wiki/Scientific_pitch_notation#Table_of_note_frequencies
+// and en.wikipedia.org/wiki/Guitar_tunings#Standard
+function getStringAndFret(note: number): StringFret {
+    const tuning: number[] = [64, 59, 55, 50, 45, 40]; // Standard tuning
+    for (var i = 0; i < tuning.length; ++i) {
+        if (note >= tuning[i]) {
+            return { string_idx: i, fret: note - tuning[i] };
+        }
+    }
+    throw new Error("Could not determine string and fret of note (note index: " + note + ")");
+}
+
+// Get indices of the passed array which are local maxima
+function getLocalMaximaIndices(values: number[]): number[] {
+    // handle edge cases
+    if (values.length == 0) return [];
+    if (values.length == 1) return [0];
+
+    const indices: number[] = [];
+
+    // handle first index being a local maxima
+    if (values[0] >= values[1]) {
+        indices.push(0);
+    }
+    // handle middle values being a local maxima
+    for (var i = 1; i < values.length - 1; ++i) {
+        // use >= for next value in order to get only the first of equal values
+        if (values[i] > values[i - 1] && values[i] >= values[i + 1]) {
+            indices.push(i);
+        }
+    }
+    // handle last index being a local maxima
+    if (values[values.length-1] > values[values.length-2]) {
+        indices.push(values.length-1);
+    }
+
+    return indices;
+}
+
 export default async function tabLick(lick: Lick): Promise<void> {
     console.log("tabbing lick with crepe.");
     console.log(lick);
@@ -33,6 +95,25 @@ export default async function tabLick(lick: Lick): Promise<void> {
 
     console.log("final audio data:");
     console.log(data);
+
+    const peakIndices: number[] = getLocalMaximaIndices(data.peakAmplitude);
+    const peakFrequencies: number[] = peakIndices.map(idx => data.frequency[idx]);
+    const peakNotes: number[] = peakFrequencies.map(freq => getNote(freq));
+    const peakStringsAndFrets: StringFret[] = peakNotes.map(note => getStringAndFret(note));
+    // 10-millisecond samples
+    console.log("Processed audio data:");
+    console.log("Time\tIndex\tFrequency\tAmplitude\tNote\tString\tFret")
+    for (var i = 0; i < peakIndices.length; ++i) {
+        const idx: number = peakIndices[i];
+        const str: string = data.time[idx] + "\t"
+                            + idx + "\t"
+                            + peakFrequencies[i] + "\t\t"
+                            + data.peakAmplitude[idx] + "\t"
+                            + peakNotes[i] + "\t"
+                            + (peakStringsAndFrets[i].string_idx+1) + "\t"
+                            + peakStringsAndFrets[i].fret;
+        console.log(str);
+    }
 
     console.log("done tabbing lick.");
 }
