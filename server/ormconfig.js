3152e707b38c675de169ab28aba5c7fa32852de4
Isaac Cormack <isaac.m.cormack@gmail.com>
2021-01-16 06:11:48 -0800

    enable use of env variables in ormconfig to swap between dev and prod db

--- server/ormconfig.js
+++ server/ormconfig.js
@@ -1,9 +1,22 @@
-{
+if (process.env.NODE_ENV === 'prod') {
+    if (!process.env.PROD_DB_IP) throw new Error('process.env.PROD_DB_IP is not defined')
+    if (!process.env.PROD_DB_PASSWORD) throw new Error('process.env.PROD_DB_PASSWORD is not defined')
+}
+
+const host = {
+    local_docker: "host.docker.internal",
+    prod: process.env.PROD_DB_IP
+}
+const password = {
+    prod: process.env.PROD_DB_PASSWORD
+}
+
+module.exports = {
     "type": "mysql",
-    "host": "host.docker.internal",
+    "host": host[process.env.NODE_ENV] || "localhost",
     "port": 3306,
     "username": "root",
-    "password": "helloworld",
+    "password": password[process.env.NODE_ENV] || "helloworld",
     "database": "tabber",
     "synchronize": true,
     "controllers": [
